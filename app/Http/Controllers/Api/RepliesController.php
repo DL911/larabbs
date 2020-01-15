761d995e49b013a9478aa43eca8d2eda37e051ee
Dlong <dlong1011@qq.com>
2020-01-15 03:04:57 +0000

    删除回复

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -13,11 +13,24 @@ class RepliesController extends Controller
 {
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
-        $reply->content = $request->content;
+        //$reply->content = $request->content;
+        $reply->content = $request->all()['content'];
         $reply->topic()->associate($topic);
         $reply->user()->associate($request->user());
         $reply->save();
 
         return new ReplyResource($reply);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            abort(404);
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return response(null, 204);
     }
 }
