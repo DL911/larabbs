00a84ca0e5d4fb89d34cd61a95196cb61e60ac3c
Dlong <dlong1011@qq.com>
2020-01-15 07:39:14 +0000

    本地化

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorzationRequest;
 use App\Models\User;
@@ -72,7 +71,8 @@ class AuthorizationsController extends Controller
 
         $credentials['password'] = $request->password;
         if (!$token = Auth::guard('api')->attempt($credentials)) {
-            throw new AuthenticationException('用户名或密码错误');
+            throw new AuthenticationException(trans('auth.failed'));
+            //throw new AuthenticationException('用户名或密码错误');
         }
 
         return $this->respondWithToken($token)->setStatusCode(201);
--- app/Http/Controllers/Api/CaptchasController.php
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\CaptchasRequest;
 use Gregwar\Captcha\CaptchaBuilder;
 use Illuminate\Http\Request;
